33b7a27974bb7414b4ca4f009967c677fb7876f9
Danmwihoti <danmwihoti@gmail.com>
2024-07-18 15:57:04 +0300

    omnichain contract

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -3,13 +3,25 @@ pragma solidity 0.8.7;
 
 import "@zetachain/protocol-contracts/contracts/zevm/SystemContract.sol";
 import "@zetachain/protocol-contracts/contracts/zevm/interfaces/zContract.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@zetachain/toolkit/contracts/BytesHelperLib.sol";
 import "@zetachain/toolkit/contracts/OnlySystem.sol";
 
-contract NFT is zContract, OnlySystem {
+contract NFT is zContract, ERC721, OnlySystem {
     SystemContract public systemContract;
+    error  CallerNotOwnerNotApproved();
+    uint256 constant BITCOIN = 18332;
 
-    constructor(address systemContractAddress) {
+
+    mapping(uint256 => uint256) public tokenAmounts;
+    mapping(uint256 => uint256) public tokenChains;
+
+    uint256 private _nextTokenId;
+
+
+    constructor(address systemContractAddress)  ERC721("MYNFT", "MNFT"){
         systemContract = SystemContract(systemContractAddress);
+        _nextTokenId = 0;
     }
 
     function onCrossChainCall(
@@ -17,11 +29,15 @@ contract NFT is zContract, OnlySystem {
         address zrc20,
         uint256 amount,
         bytes calldata message
-    ) external virtual override onlySystem(systemContract) {
-        (address recipient) = abi.decode(
-            message,
-            (address)
-        );
-        // TODO: implement the logic
+    ) external override onlySystem(systemContract) {
+        address recipient;
+        
+        if (context.chainID == BITCOIN) {
+            recipient = BytesHelperLib.bytesToAddress(message, 0);
+        }
+        else {
+            recipient = abi.decode(message, (address));
+        }
+        _mintNFT(recipient, context.chainID, amount);
     }
 }
